fd24bb7c01ff037f94d1108ddaec7faeefbc9d04
Durga Prasad Gatti <durgaprasadgatti1996@gmail.com>
2018-12-17 13:18:05 +0530

    Removed parent and size arrays from function arguments

--- data structures/dsu.cpp
+++ data structures/dsu.cpp
@@ -1,33 +1,34 @@
-int root(int Arr[], int i)
+
+int root(int i)
 {
-	while (Arr[i] != i)
+	while (par[i] != i)
 	{
-		Arr[i] = Arr[Arr[i]];
-		i = Arr[i];
+		par[i] = par[par[i]];
+		i = par[i];
 	}
 	return i;
 }
 
-void wunion(int Arr[], int Size[], int A, int B)
+void wunion(int A, int B)
 {
-	int root_A = root(Arr, A);
-	int root_B = root(Arr, B);
+	int root_A = root(A);
+	int root_B = root(B);
 	if (Size[root_A] < Size[root_B])
 	{
-		Arr[root_A] = Arr[root_B];
+		par[root_A] = par[root_B];
 		Size[root_B] += Size[root_A];
 	}
 	else
 	{
-		Arr[root_B] = Arr[root_A];
+		par[root_B] = par[root_A];
 		Size[root_A] += Size[root_B];
 	}
 
 }
 
-bool find(int Arr[], int A, int B)
+bool Find(int A, int B)
 {
-	if (root(Arr, A) == root(Arr, B))       //if A and B have same root,means they are connected.
+	if (root(A) == root(B))       //if A and B have same root,means they are connected.
 		return true;
 	else
 		return false;
