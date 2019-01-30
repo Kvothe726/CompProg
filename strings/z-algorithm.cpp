6174953eecb285eef7a70fd8d38b4152d2712b40
Durga Prasad Gatti <durgaprasadgatti1996@gmail.com>
2019-01-31 02:04:34 +0530

    Changed z algo to class implementation

--- strings/z-algorithm.cpp
+++ strings/z-algorithm.cpp
@@ -1,23 +1,33 @@
-int n;
-string s;
-int z[nmax];
-
-void computez(){
-    int L = 0, R = 0;
-    for (int i = 1; i < n; i++) {
-        if (i > R) {
-            L = R = i;
-            while (R < n && s[R-L] == s[R]) R++;
-            z[i] = R-L; R--;
-        } else {
-            int k = i-L;
-            if (z[k] < R-i+1) z[i] = z[k];
+class zalgo {
+public:
+    string s;
+    int n;
+    int z[nmax];
+    zalgo(string p) {
+        memset(z, 0, sizeof(z));
+        s = p;
+        n = p.size();
+        computez();
+    }
+    void computez() {
+        z[0]=n;
+        int L = 0, R = 0;
+        for (int i = 1; i < n; i++) {
+            if (i > R) {
+                L = R = i;
+                while (R < n && s[R - L] == s[R]) R++;
+                z[i] = R - L; R--;
+            }
             else {
-                L = i;
-                while (R < n && s[R-L] == s[R]) R++;
-                z[i] = R-L; R--;
+                int k = i - L;
+                if (z[k] < R - i + 1) z[i] = z[k];
+                else {
+                    L = i;
+                    while (R < n && s[R - L] == s[R]) R++;
+                    z[i] = R - L; R--;
+                }
             }
         }
     }
-    return;
-}
+
+};
