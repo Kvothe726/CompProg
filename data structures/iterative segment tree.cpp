31e5541db723f47a9fad8ab2d7787a9a5be7c55d
DurgaPrasad <durgaprasadgatti1996@gmail.com>
2019-11-10 16:18:43 +0530

    changed the max array size to nmax

--- data structures/iterative segment tree.cpp
+++ data structures/iterative segment tree.cpp
@@ -1,6 +1,5 @@
-const int N = 1e5;  // limit for array size
 int n;  // array size
-int t[2 * N];
+int t[2 * nmax];
 
 void build() {  // build the tree
   for (int i = n - 1; i > 0; --i) t[i] = t[i<<1] + t[i<<1|1];
