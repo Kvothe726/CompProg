8344a1060c4ba789201a858f89f550521707f3b4
DurgaPrasad <durgaprasadgatti1996@gmail.com>
2019-12-30 19:52:48 +0530

    euler phi sieve

--- math/sieve.cpp
+++ math/sieve.cpp
@@ -1,20 +1,20 @@
-int prime[MAXV];
+int prime[nmax];
 vector <int> primes;
 
 void sieve()
 {
-    for(int i=0;i<MAXV;i++) {
+    for(int i=0;i<nmax;i++) {
         prime[i] = i;
     }
-    for (int p=2; p*p<=(MAXV-1); p++)
+    for (int p=2; p*p<=(nmax-1); p++)
     {
         if (prime[p] == p)
         {
-            for (int i=p*p; i<=(MAXV-1); i += p)
+            for (int i=p*p; i<=(nmax-1); i += p)
                 prime[i] = p;
         }
     }
-    for(int i=2;i<MAXV;i++){
+    for(int i=2;i<nmax;i++){
         if(prime[i]==i){
             primes.push_back(i);
         }
@@ -50,4 +50,28 @@ void mobsieve(){
     }
 
     return;
+}
+
+vector <int> prime;
+bool is_composite[nmax];
+int phi[nmax];
+
+void sieve () {
+	std::fill (is_composite, is_composite + nmax, false);
+	phi[1] = 1;
+	for (int i = 2; i < nmax; ++i) {
+		if (!is_composite[i]) {
+			prime.push_back (i);
+			phi[i] = i - 1;					//i is prime
+		}
+		for (int j = 0; j < prime.size () && i * prime[j] < nmax; ++j) {
+			is_composite[i * prime[j]] = true;
+			if (i % prime[j] == 0) {
+				phi[i * prime[j]] = phi[i] * prime[j];	//prime[j] divides i
+				break;
+			} else {
+				phi[i * prime[j]] = phi[i] * phi[prime[j]];	//prime[j] does not divide i
+			}
+		}
+	}
 }
