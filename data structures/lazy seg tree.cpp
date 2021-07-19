3a7c977fb5af9d59fcb88b9e2e0dd603e5acd351
prasad <durgaprasadgatti1996@gmail.com>
2021-07-20 00:35:34 +0530

    generalized lazy segment tree

--- data structures/lazy seg tree.cpp
+++ data structures/lazy seg tree.cpp
@@ -1,62 +1,204 @@
-/*If in any problem you want to use any data structure two times (ex. two seg trees in the same problem) just create a class for it and create two object instances*/
-// This is for min over segment, with addition update.
+template <class S,
+          S (*op)(S, S),
+          S (*e)(),
+          class F,
+          S (*mapping)(F, S),
+          F (*composition)(F, F),
+          F (*id)()>
+struct lazy_segtree {
+    int ceil_pow2(int n) {
+        int x = 0;
+        while ((1U << x) < (unsigned int)(n)) x++;
+        return x;
+    }
+  public:
+    lazy_segtree() : lazy_segtree(0) {}
+    lazy_segtree(int n) : lazy_segtree(std::vector<S>(n, e())) {}
+    lazy_segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        lz = std::vector<F>(size, id());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
 
-#include <iostream>
-#include <climits>
-#define nmax 100005
-using namespace std;
-int st[4*nmax],lazy[4*nmax]={0};
-void buildst(int *a,int start,int end,int node){
-	if(start==end)st[node]=a[start];
-	else if(start<end){
-		int mid=(start+end)/2,left=2*node+1,right=left+1;
-		buildst(a,start,mid,left);
-		buildst(a,mid+1,end,right);
-		st[node]=min(st[left],st[right]);
-	}
-}
-void update(int start,int end,int l,int r,int dif,int node){
-	if(lazy[node]!=0){
-		// there are pending updates
-		st[node]+=lazy[node];
-		if(start!=end){
-			lazy[2*node+1]+=lazy[node];
-			lazy[2*node+2]+=lazy[node];
-		}
-		lazy[node]=0;
-	}
-	if(start> r or end<l)return;
-	else if(start>=l and end<=r){
-		st[node]+=dif;	// make the update for the current range and keep them pending for the child nodes
-		if(start!=end){
-			lazy[2*node+1]+=dif;
-			lazy[2*node+2]+=dif;
-		}
-		return;
-	}
-	int mid=(start+end)/2;
-	update(start,mid,l,r,dif,2*node+1);
-	update(mid+1,end,l,r,dif,2*node+2);
-	st[node]=min(st[2*node+1],st[2*node+2]);
-}
-int queryMin(int start,int end,int l,int r,int node){
-	if(lazy[node]!=0){
-		// if there are updates pending, apply them before making the queries
-		st[node]+=lazy[node];
-		if(start!=end){
-			lazy[2*node+1]+=lazy[node];
-			lazy[2*node+2]+=lazy[node];
-		}
-		lazy[node]=0;
-	}
-	if(start>r or end<l)return INT_MAX-1;
-	else if(start>=l and end<=r)return st[node];
-	else{
-		int mid=(start+end)/2,left=2*node+1,right=left+1;
-		return min(queryMin(start,mid,l,r,left),queryMin(mid+1,end,l,r,right));
-	}
-}
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
 
-int main(){
+    S get(int p) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        return d[p];
+    }
 
-}
+    S prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return e();
+
+        l += size;
+        r += size;
+
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push(r >> i);
+        }
+
+        S sml = e(), smr = e();
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+
+        return op(sml, smr);
+    }
+
+    S all_prod() { return d[1]; }
+
+    void apply(int p, F f) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = mapping(f, d[p]);
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+    void apply(int l, int r, F f) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return;
+
+        l += size;
+        r += size;
+
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+
+        {
+            int l2 = l, r2 = r;
+            while (l < r) {
+                if (l & 1) all_apply(l++, f);
+                if (r & 1) all_apply(--r, f);
+                l >>= 1;
+                r >>= 1;
+            }
+            l = l2;
+            r = r2;
+        }
+
+        for (int i = 1; i <= log; i++) {
+            if (((l >> i) << i) != l) update(l >> i);
+            if (((r >> i) << i) != r) update((r - 1) >> i);
+        }
+    }
+
+    template <bool (*g)(S)> int max_right(int l) {
+        return max_right(l, [](S x) { return g(x); });
+    }
+    template <class G> int max_right(int l, G g) {
+        assert(0 <= l && l <= _n);
+        assert(g(e()));
+        if (l == _n) return _n;
+        l += size;
+        for (int i = log; i >= 1; i--) push(l >> i);
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(op(sm, d[l]))) {
+                while (l < size) {
+                    push(l);
+                    l = (2 * l);
+                    if (g(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+
+    template <bool (*g)(S)> int min_left(int r) {
+        return min_left(r, [](S x) { return g(x); });
+    }
+    template <class G> int min_left(int r, G g) {
+        assert(0 <= r && r <= _n);
+        assert(g(e()));
+        if (r == 0) return 0;
+        r += size;
+        for (int i = log; i >= 1; i--) push((r - 1) >> i);
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(op(d[r], sm))) {
+                while (r < size) {
+                    push(r);
+                    r = (2 * r + 1);
+                    if (g(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+    std::vector<F> lz;
+
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+    void all_apply(int k, F f) {
+        d[k] = mapping(f, d[k]);
+        if (k < size) lz[k] = composition(f, lz[k]);
+    }
+    void push(int k) {
+        all_apply(2 * k, lz[k]);
+        all_apply(2 * k + 1, lz[k]);
+        lz[k] = id();
+    }
+};
+
+// main element whether an int or a set
+struct S {
+    mint a;
+    int size;
+};
+
+// lazy array element
+struct F {
+    mint a, b;
+};
+
+// range query operation
+S op(S l, S r) { return S{l.a + r.a, l.size + r.size}; }
+
+// main identity element such that op(e(), S) will be S
+S e() { return S{0, 0}; }
+
+// apply the lazy element F to main element S
+S mapping(F l, S r) { return S{r.a * l.a + r.size * l.b, r.size}; }
+
+// Combine two lazy elements
+F composition(F l, F r) { return F{r.a * l.a, r.b * l.a + l.b}; }
+
+// lazy identity element such that composition(id(), F) will be F
+F id() { return F{1, 0}; }
