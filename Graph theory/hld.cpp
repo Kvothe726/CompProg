042046b10e6065c27c2f70aafa182fdfa3114604
prasad <durgaprasadgatti1996@gmail.com>
2021-09-02 17:03:27 +0530

    hld with generalized seg tree

---
+++ Graph theory/hld.cpp
@@ -0,0 +1,291 @@
+template <class S,
+          S (*op)(S, S),
+          S (*e)(),
+          class F,
+          S (*mapping)(F, S),
+          F (*composition)(F, F),
+          F (*id)()>
+struct lazy_segtree {
+    int ceil_pow2(int n) {
+        int x = 0;
+        while ((1U << x) < (unsigned int)(n)) x++;
+        return x;
+    }
+  public:
+    lazy_segtree() : lazy_segtree(0) {}
+    lazy_segtree(int n) : lazy_segtree(std::vector<S>(n, e())) {}
+    lazy_segtree(const std::vector<S>& v) : _n(int(v.size())) {
+        log = ceil_pow2(_n);
+        size = 1 << log;
+        d = std::vector<S>(2 * size, e());
+        lz = std::vector<F>(size, id());
+        for (int i = 0; i < _n; i++) d[size + i] = v[i];
+        for (int i = size - 1; i >= 1; i--) {
+            update(i);
+        }
+    }
+ 
+    void set(int p, S x) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = x;
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+ 
+    S get(int p) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        return d[p];
+    }
+ 
+    S prod(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return e();
+ 
+        l += size;
+        r += size;
+ 
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push(r >> i);
+        }
+ 
+        S sml = e(), smr = e();
+        while (l < r) {
+            if (l & 1) sml = op(sml, d[l++]);
+            if (r & 1) smr = op(d[--r], smr);
+            l >>= 1;
+            r >>= 1;
+        }
+ 
+        return op(sml, smr);
+    }
+ 
+    S all_prod() { return d[1]; }
+ 
+    void apply(int p, F f) {
+        assert(0 <= p && p < _n);
+        p += size;
+        for (int i = log; i >= 1; i--) push(p >> i);
+        d[p] = mapping(f, d[p]);
+        for (int i = 1; i <= log; i++) update(p >> i);
+    }
+    void apply(int l, int r, F f) {
+        assert(0 <= l && l <= r && r <= _n);
+        if (l == r) return;
+ 
+        l += size;
+        r += size;
+ 
+        for (int i = log; i >= 1; i--) {
+            if (((l >> i) << i) != l) push(l >> i);
+            if (((r >> i) << i) != r) push((r - 1) >> i);
+        }
+ 
+        {
+            int l2 = l, r2 = r;
+            while (l < r) {
+                if (l & 1) all_apply(l++, f);
+                if (r & 1) all_apply(--r, f);
+                l >>= 1;
+                r >>= 1;
+            }
+            l = l2;
+            r = r2;
+        }
+ 
+        for (int i = 1; i <= log; i++) {
+            if (((l >> i) << i) != l) update(l >> i);
+            if (((r >> i) << i) != r) update((r - 1) >> i);
+        }
+    }
+ 
+    template <bool (*g)(S)> int max_right(int l) {
+        return max_right(l, [](S x) { return g(x); });
+    }
+    template <class G> int max_right(int l, G g) {
+        assert(0 <= l && l <= _n);
+        assert(g(e()));
+        if (l == _n) return _n;
+        l += size;
+        for (int i = log; i >= 1; i--) push(l >> i);
+        S sm = e();
+        do {
+            while (l % 2 == 0) l >>= 1;
+            if (!g(op(sm, d[l]))) {
+                while (l < size) {
+                    push(l);
+                    l = (2 * l);
+                    if (g(op(sm, d[l]))) {
+                        sm = op(sm, d[l]);
+                        l++;
+                    }
+                }
+                return l - size;
+            }
+            sm = op(sm, d[l]);
+            l++;
+        } while ((l & -l) != l);
+        return _n;
+    }
+ 
+    template <bool (*g)(S)> int min_left(int r) {
+        return min_left(r, [](S x) { return g(x); });
+    }
+    template <class G> int min_left(int r, G g) {
+        assert(0 <= r && r <= _n);
+        assert(g(e()));
+        if (r == 0) return 0;
+        r += size;
+        for (int i = log; i >= 1; i--) push((r - 1) >> i);
+        S sm = e();
+        do {
+            r--;
+            while (r > 1 && (r % 2)) r >>= 1;
+            if (!g(op(d[r], sm))) {
+                while (r < size) {
+                    push(r);
+                    r = (2 * r + 1);
+                    if (g(op(d[r], sm))) {
+                        sm = op(d[r], sm);
+                        r--;
+                    }
+                }
+                return r + 1 - size;
+            }
+            sm = op(d[r], sm);
+        } while ((r & -r) != r);
+        return 0;
+    }
+ 
+  private:
+    int _n, size, log;
+    std::vector<S> d;
+    std::vector<F> lz;
+ 
+    void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+    void all_apply(int k, F f) {
+        d[k] = mapping(f, d[k]);
+        if (k < size) lz[k] = composition(f, lz[k]);
+    }
+    void push(int k) {
+        all_apply(2 * k, lz[k]);
+        all_apply(2 * k + 1, lz[k]);
+        lz[k] = id();
+    }
+};
+ 
+template <class S,
+S (*op)(S, S),
+S (*e)(),
+class F,
+S (*mapping)(F, S),
+F (*composition)(F, F),
+F (*id)()>
+class HLD {
+public:
+    HLD(vector<vector<int>> const& adj, int root):adj(adj),root(root) {
+        int n = adj.size();
+        parent = vector<int>(n);
+        depth = vector<int>(n);
+        heavy = vector<int>(n, -1);
+        head = vector<int>(n);
+        pos = vector<int>(n);
+        cur_pos = 0;
+        st = lazy_segtree<S,op,e,F,mapping,composition,id> (n);
+ 
+        dfs(root);
+        decompose(root, 0);
+    }
+ 
+    int dfs(int v) {
+        int size = 1;
+        int max_c_size = 0;
+        for (int c : adj[v]) {
+            if (c != parent[v]) {
+                parent[c] = v, depth[c] = depth[v] + 1;
+                int c_size = dfs(c);
+                size += c_size;
+                if (c_size > max_c_size)
+                    max_c_size = c_size, heavy[v] = c;
+            }
+        }
+        return size;
+    }
+ 
+    void decompose(int v, int h) {
+        head[v] = h, pos[v] = cur_pos++;
+        if (heavy[v] != -1)
+            decompose(heavy[v], h);
+        for (int c : adj[v]) {
+            if (c != parent[v] && c != heavy[v])
+                decompose(c, c);
+        }
+    }
+ 
+    S query(int a, int b) {
+        S res = e();
+        for (; head[a] != head[b]; b = parent[head[b]]) {
+            if (depth[head[a]] > depth[head[b]])
+                swap(a, b);
+            S cur_heavy_path_max = st.prod(pos[head[b]], pos[b]+1);
+            res = op(res, cur_heavy_path_max);
+        }
+        if (depth[a] > depth[b])
+            swap(a, b);
+        S last_heavy_path_max = st.prod(pos[a], pos[b]+1);
+        res = op(res, last_heavy_path_max);
+        return res;
+    }
+    
+    void apply(int a, int b, F f) {
+        for (; head[a] != head[b]; b = parent[head[b]]) {
+            if (depth[head[a]] > depth[head[b]])
+                swap(a, b);
+            st.apply(pos[head[b]], pos[b]+1, f);
+        }
+        if (depth[a] > depth[b])
+            swap(a, b);
+        st.apply(pos[a]+1, pos[b]+1, f);
+        return;
+    }
+ 
+    lazy_segtree<S,op,e,F,mapping,composition,id> st;
+    vector<vector<int>> adj;
+    vector<int> parent, depth, heavy, head, pos;
+    int root,cur_pos;
+};
+ 
+// for any f and any x,y f(op(x,y)) = op(f(x),f(y))
+
+// main element whether an int or a set
+using S = ll;
+ 
+// lazy array element
+using F = ll;
+ 
+// range query operation
+S op(S l, S r) { return min(l,r); }
+ 
+// main identity element such that op(e(), S) will be S
+S e() { return 1e9+5; }
+ 
+// apply the lazy element F to main element S, check if mapping(id(),S) is S
+S mapping(F l, S r) {
+    if(l==1e9+5)
+        return r;
+    return l;
+}
+ 
+// Combine two lazy elements such that l is the last one
+F composition(F l, F r) {
+    if(l == 1e9+5){
+        return r;
+    }
+    return l;
+}
+ 
+// lazy identity element such that composition(F, id()), composition(id(), F) will be F and also mapping(id(),S) will be S
+F id() { return 1e9+5; }
