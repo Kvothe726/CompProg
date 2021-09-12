c981a71b3344cc9bf364a4cfa6a28b64e312f88f
prasad <durgaprasadgatti1996@gmail.com>
2021-09-12 16:01:58 +0530

    dsu on a tree

---
+++ Graph theory/tree path merge sample.cpp
@@ -0,0 +1,17 @@
+// this code is just a sample for understanding
+// We maintain two data structures one is the original tree as par and
+// A dsu that merges a path in the tree, and root of each component is the highest node from bottom.
+// To update on a path(u,v) leaving already updated edges, let l = lca(u,v) and do paint(u,l,val,ans) and same for v
+// imagine a tree path (node -> ancestor) as a path with bunch of already connected components in middle.
+
+par[19][MAXN];
+int pa[MAXN], ans[MAXN];
+ 
+int paint(int s, int l, int v, int *ans){
+	if(s != pa[s]) return pa[s] = paint(pa[s], l, v, ans);
+	if(dep[s] <= dep[l]) return s;
+	else{
+		ans[s] = v;
+		return pa[s] = paint(par[0][s], l, v, ans);
+	}
+}
