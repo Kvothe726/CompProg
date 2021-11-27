8e264fd6468593d8cb5f932482051217fcbdf94e
prasad <durgaprasadgatti1996@gmail.com>
2021-11-27 23:50:41 +0530

    convex hull trick comments and some small corrections

--- dp/convexhulltrick.cpp
+++ dp/convexhulltrick.cpp
@@ -1,5 +1,7 @@
+// remember lines that are added should be in decreasing order of slope.
+// (look the lines only at the 1st and 4th quadrants to understand decreasing slope).
 struct line{
-    int m;
+    ll m;
     ll c;
 };
 
@@ -8,21 +10,34 @@ public:
     int n=0;
     vector <ll> st;
     vector <line> lines;
+    // need an initial line to set the starting point of that line
     convexhull(line a){
         st.push_back(0);
         lines.push_back(a);
         n++;
     }
     ll get(line &a, ll x){
-        return 1ll*a.m*x+a.c;
-    }
-    ll getceil(ll p, int q){
-        p=abs(p),q=abs(q);
-        return (p+q-1)/q;
+        return a.m*x+a.c;
     }
     bool isgreat(line &a, line &b, ll x){
-        return x>=getceil(b.c-a.c,a.m-b.m);
+        return a.m*x+a.c >= b.m*x+b.c;
+    }
+    getceil(ll num, ll den){
+        if(num == 0){
+            return 0;
+        }
+        assert(den!=0);
 
+        int sign = (num/abs(num))*(den/abs(den));
+        num = abs(num);
+        den = abs(den);
+
+        if(sign == 1){
+            return (num+den-1)/den;
+        }
+        else{
+            return -(num/den);
+        }
     }
     void update(line a){
         for(int i=n-1;i>=0;i--){
@@ -38,13 +53,16 @@ public:
         ll xint=0;
         if(lines.size()){
             line la = lines.back();
+            if(a.m == la.m){
+                return;
+            }
             xint = getceil((la.c-a.c),(a.m-la.m));
         }
         st.push_back(xint);
         lines.push_back(a);
         n++;
     }
-    ll getmin(int x){
+    ll getmin(ll x){
         auto it = upper_bound(st.begin(),st.end(),x)-st.begin()-1;
         return get(lines[it],x);
     }
