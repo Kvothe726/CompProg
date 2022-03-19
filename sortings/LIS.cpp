6c44282215afd092a914e29f189646de197d82b8
prasad <durgaprasadgatti1996@gmail.com>
2022-03-19 07:23:35 +0530

    minor initializations

--- sortings/LIS.cpp
+++ sortings/LIS.cpp
@@ -2,8 +2,9 @@
 
 void LIS(vector<int> a, vector<int>& dp){ //dp[i] max non dec subsequence ending at i.
 	int n=a.size();
-	vector<int> d(n+1,1e9);
-	d[0]=-1e9;
+	dp.resize(n);
+	vector<int> d(n+1,2e9);
+	d[0]=-2e9;
 
 	for(int i=0;i<n;i++){
 		auto it=upper_bound(d.begin(),d.end(),a[i]); // for strictly increasing change it to lower_bound
